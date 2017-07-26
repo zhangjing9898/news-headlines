1eea00649efe246f89eb08420b8c42f5ce45c280
zhangjing9898 <1105249307@qq.com>
2017-07-26 11:40:20 +0800

    some update this vision is eventually vision
    some update this vision is eventually vision

--- src/js/components/common_comments.js
+++ src/js/components/common_comments.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Row, Col} from 'antd';
 import {
-	Menu,
-	Icon,
-	Tabs,
-	message,
-	Form,
-	Input,
-	Button,
-	CheckBox,
-	Modal,
-	Card,
-	notification
+    Menu,
+    Icon,
+    Tabs,
+    message,
+    Form,
+    Input,
+    Button,
+    CheckBox,
+    Modal,
+    Card,
+    notification
 } from 'antd';
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
@@ -19,83 +19,68 @@ const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
 import {Router, Route, Link, browserHistory} from 'react-router'
 class CommonComments extends React.Component {
-	constructor() {
+    constructor() {
         super();
         this.state = {
             comments: ''
         };
     };
-	componentDidMount() {
-		var myFetchOptions = {
-			method: 'GET'
-		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey="
-		 + this.props.uniquekey, myFetchOptions)
-		 .then(response => response.json())
-		 .then(json => {
-			this.setState({comments: json});
-		});
-	};
-	handleSubmit(e) {
-		e.preventDefault();
-		var myFetchOptions = {
-			method: 'GET'
-		};
-		var formdata = this.props.form.getFieldsValue();
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid="
-		 + localStorage.userid
-		 + "&uniquekey=" + this.props.uniquekey
-		  + "&commnet=" + formdata.remark, myFetchOptions).then(response => response.json()).then(json => {
-			this.componentDidMount();
-		})
-	};
-
-	addUsrCollection(){
-		var myFetchOptions = {
-			method:'GET'
-		};
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid=" + localStorage.userid
-		+ "&uniquekey=" + this.props.uniquekey,myFetchOptions)
-		.then(response => response.json())
-		.then(json => {
-			//收藏成功以后进行一次全局的提醒
-			notification['success']({
-				message:'ReactNews提醒',
-				description:'收藏此文章成功'
-			});
-		})
-	}
-
-	render() {
-		let {getFieldDecorator} = this.props.form;
-		const {comments} = this.state;
-		let length=comments.length;
-		let newComments=comments.slice(length-10,length);
-		const commnetList = newComments.length
-			? newComments.map((comment, index) => (
-					<Card key={index} title={comment.UserName} extra={<a href = "#"> 发布于 {comment.datetime} </a>}>
-						<p>{comment.Comments}</p>
-					</Card>
-					))
-			: '没有加载到任何评论';
-		return (
+    componentDidMount() {
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+            this.setState({comments: json});
+        });
+    };
+    handleSubmit(e) {
+        e.preventDefault();
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formdata = this.props.form.getFieldsValue();
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=comment&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey + "&commnet=" + formdata.remark, myFetchOptions).then(response => response.json()).then(json => {
+            this.componentDidMount();
+        })
+    };
+    addUserCollection() {
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
+            //收藏成功以后进行一下全局的提醒
+            notification['success']({message: 'ReactNews提醒', description: '收藏此文章成功'});
+        });
+    };
+    render() {
+        let {getFieldProps} = this.props.form;
+        const {comments} = this.state;
+        let commentLength=comments.length;
+        let newComment = commentLength > 10 ? comments.slice(commentLength-10,commentLength) : comments;
+        const commnetList = comments.length
+            ? newComment.map((comment, index) => (
+				<Card key={index} title={comment.UserName} extra={< a href = "#" > 发布于 {comment.datetime} </a>}>
+					<p>{comment.Comments}</p>
+				</Card>
+            ))
+            : '没有加载到任何评论';
+        return (
 			<div class="comment">
 				<Row>
 					<Col span={24}>
-						{commnetList}
+                        {commnetList}
 						<Form onSubmit ={this.handleSubmit.bind(this)}>
 							<FormItem label="您的评论">
-								<Input type="textarea" placeholder="随便写" {...getFieldDecorator('remark',{initialValue: ''})}/>
+								<Input type="textarea" placeholder="随便写" {...getFieldProps('remark',{initialValue: ''})}/>
 							</FormItem>
 							<Button type="primary" htmlType="submit">提交评论</Button>
 							&nbsp;&nbsp;
-							<Button type="primary" htmlType="button" onClick={this.addUsrCollection.bind(this)}>收藏该文章</Button>
+							<Button type="primary" htmlType="button" onClick={this.addUserCollection.bind(this)}>收藏该文章</Button>
 						</Form>
 					</Col>
 				</Row>
 			</div>
-		);
-	};
+        );
+    };
 }
-
 export default CommonComments = Form.create({})(CommonComments);
--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,112 +1,115 @@
 import React from 'react';
 import {Row, Col} from 'antd';
 import {
-	Menu,
-	Icon,
-	Tabs,
-	message,
-	Form,
-	Input,
-	Button,
-	CheckBox,
-	Modal
+    Menu,
+    Icon,
+    Tabs,
+    message,
+    Form,
+    Input,
+    Button,
+    CheckBox,
+    Modal
 } from 'antd';
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
 const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
+import {Router, Route, Link, browserHistory} from 'react-router'
 class PCHeader extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			current: 'top',
-			modalVisible: false,
-			action: 'login',
-			hasLogined: false,
-			userNickName: '',
-			userid: 0
-		};
-	};
+    constructor() {
+        super();
+        this.state = {
+            current: 'top',
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0
+        };
+    };
 
-	componentWillMount(){
-		if (localStorage.userid!='') {
-			this.setState({hasLogined:true});
-			this.setState({userNickName:localStorage.userNickName,userid:localStorage.userid});
-		}
-	};
+    componentWillMount(){
+        if (localStorage.userid!='') {
+            this.setState({hasLogined:true});
+            this.setState({userNickName:localStorage.userNickName,userid:localStorage.userid});
+        }
+    };
 
-	setModalVisible(value)
-	{
-		this.setState({modalVisible: value});
-	};
-	handleClick(e) {
-		if (e.key == "register") {
-			this.setState({current: 'register'});
-			this.setModalVisible(true);
-		} else {
-			{
-				this.setState({current: e.key});
-			}
-		}
-	};
-	handleSubmit(e)
-	{
-		//页面开始向 API 进行提交数据
-		e.preventDefault();
-		var myFetchOptions = {
-			method: 'GET'
-		};
-		var formData = this.props.form.getFieldsValue();
-		console.log(formData);
-		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
-		+ "&username="+formData.userName+"&password="+formData.password
-		+"&r_userName=" + formData.r_userName + "&r_password="
-		+ formData.r_password + "&r_confirmPassword="
-		+ formData.r_confirmPassword, myFetchOptions)
-		.then(response => response.json())
-		.then(json => {
-			this.setState({userNickName: json.NickUserName, userid: json.UserId});
-			localStorage.userid= json.UserId;
-			localStorage.userNickName = json.NickUserName;
-		});
-		if (this.state.action=="login") {
-			this.setState({hasLogined:true});
-		}
-		message.success("请求成功！");
-		this.setModalVisible(false);
-	};
-	callback(key) {
-		if (key == 1) {
-			this.setState({action: 'login'});
-		} else if (key == 2) {
-			this.setState({action: 'register'});
-		}
-	};
-	logout(){
-		localStorage.userid= '';
-		localStorage.userNickName = '';
-		this.setState({hasLogined:false});
-	};
-	render() {
-		const { getFieldDecorator } = this.props.form;
-		const userShow = this.state.hasLogined
-			? <Menu.Item key="logout" class="register">
-					<Button type="primary" htmlType="button">{this.state.userNickName}</Button>
-					&nbsp;&nbsp;
-						<Button type="dashed" htmlType="button">个人中心</Button>
-					&nbsp;&nbsp;
-					<Button type="ghost" htmlType="button" onClick={this.logout.bind(this)}>退出</Button>
-				</Menu.Item>
-			: <Menu.Item key="register" class="register">
+    setModalVisible(value)
+    {
+        this.setState({modalVisible: value});
+    };
+    handleClick(e) {
+        if (e.key == "register") {
+            this.setState({current: 'register'});
+            this.setModalVisible(true);
+        } else {
+            {
+                this.setState({current: e.key});
+            }
+        }
+    };
+    handleSubmit(e)
+    {
+        //页面开始向 API 进行提交数据
+        e.preventDefault();
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        console.log(formData);
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
+            + "&username="+formData.userName+"&password="+formData.password
+            +"&r_userName=" + formData.r_userName + "&r_password="
+            + formData.r_password + "&r_confirmPassword="
+            + formData.r_confirmPassword, myFetchOptions)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({userNickName: json.NickUserName, userid: json.UserId});
+                localStorage.userid= json.UserId;
+                localStorage.userNickName = json.NickUserName;
+            });
+        if (this.state.action=="login") {
+            this.setState({hasLogined:true});
+        }
+        message.success("请求成功！");
+        this.setModalVisible(false);
+    };
+    callback(key) {
+        if (key == 1) {
+            this.setState({action: 'login'});
+        } else if (key == 2) {
+            this.setState({action: 'register'});
+        }
+    };
+    logout(){
+        localStorage.userid= '';
+        localStorage.userNickName = '';
+        this.setState({hasLogined:false});
+    };
+    render() {
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogined
+            ? <Menu.Item key="logout" class="register">
+				<Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+				&nbsp;&nbsp;
+				<Link target="_blank" to={`/usercenter`}>
+					<Button type="dashed" htmlType="button">个人中心</Button>
+				</Link>
+				&nbsp;&nbsp;
+				<Button type="ghost" htmlType="button" onClick={this.logout.bind(this)}>退出</Button>
+			</Menu.Item>
+            : <Menu.Item key="register" class="register">
 				<Icon type="appstore"/>注册/登录
 			</Menu.Item>;
-		return (
+        return (
 			<header>
 				<Row>
 					<Col span={2}></Col>
 					<Col span={4}>
 						<a href="/" class="logo">
-							<img src="/src/images/logo.png" alt="logo"/>
+							<img src="./src/images/logo.png" alt="logo"/>
 							<span>ReactNews</span>
 						</a>
 					</Col>
@@ -136,31 +139,31 @@ class PCHeader extends React.Component {
 							<Menu.Item key="shishang">
 								<Icon type="appstore"/>时尚
 							</Menu.Item>
-							{userShow}
+                            {userShow}
 						</Menu>
 						<Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText="关闭">
 							<Tabs type="card" onChange={this.callback.bind(this)}>
 								<TabPane tab="登录" key="1">
 									<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
 										<FormItem label="账户">
-											{getFieldDecorator('userName')(<Input placeholder="请输入您的账号" />)}
+											<Input placeholder="请输入您的账号" {...getFieldProps('userName')}/>
 										</FormItem>
 										<FormItem label="密码">
-											{getFieldDecorator('password')(<Input type="password" placeholder="请输入您的密码" />)}
+											<Input type="password" placeholder="请输入您的密码" {...getFieldProps('password')}/>
 										</FormItem>
 										<Button type="primary" htmlType="submit">登录</Button>
 									</Form>
 								</TabPane>
 								<TabPane tab="注册" key="2">
-									<Form layout="horizontal" onSubmit={this.handleSubmit.bind(this)}>
+									<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
 										<FormItem label="账户">
-											{getFieldDecorator('r_userName')(<Input placeholder="请输入您的账号"/>)}
+											<Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
 										</FormItem>
 										<FormItem label="密码">
-											{getFieldDecorator('r_password')(<Input type="password" placeholder="请输入您的密码" />)}
+											<Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
 										</FormItem>
 										<FormItem label="确认密码">
-											{getFieldDecorator('r_confirmPassword')(<Input type="password" placeholder="请再次输入您的密码" />)}
+											<Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
 										</FormItem>
 										<Button type="primary" htmlType="submit">注册</Button>
 									</Form>
@@ -171,7 +174,7 @@ class PCHeader extends React.Component {
 					<Col span={2}></Col>
 				</Row>
 			</header>
-		);
-	};
+        );
+    };
 }
 export default PCHeader = Form.create({})(PCHeader);
